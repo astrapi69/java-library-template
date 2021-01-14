5c8454aeb2b93a2d8d17e9c2fd73b52fa99cfba6
astrapi69 <asterios.raptis@gmx.net>
2021-01-14 12:26:41 +0100

    initial version of template

---
+++ src/main/java/io/github/astrapi69/InitialTemplate.java
@@ -0,0 +1,5 @@
+package io.github.astrapi69;
+
+public class InitialTemplate
+{
+}
---
+++ src/test/java/io/github/astrapi69/InitialTemplateTest.java
@@ -0,0 +1,8 @@
+package io.github.astrapi69;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InitialTemplateTest
+{
+
+}
