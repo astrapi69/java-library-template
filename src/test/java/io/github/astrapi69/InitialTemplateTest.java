84d6ace4086a3d1214f1b8e957d21758d36f6aab
astrapi69 <asterios.raptis@gmx.net>
2021-01-14 20:18:19 +0100

    initial version of unit test that can rename and adapt to new project name after using this template project

--- src/test/java/io/github/astrapi69/InitialTemplateTest.java
+++ src/test/java/io/github/astrapi69/InitialTemplateTest.java
@@ -2,7 +2,46 @@ package io.github.astrapi69;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import io.github.astrapi69.modify.ModifyFileExtensions;
+import io.github.astrapi69.search.PathFinder;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
 class InitialTemplateTest
 {
+	public static final String BUILD_GRADLE_FILENAME = "build.gradle";
+	public static final String DOT_GITHUB_DIRECTORY_NAME = ".github";
+	public static final String CODE_OF_CONDUCT_FILENAME = "CODE_OF_CONDUCT.md";
+
+	@Test
+//	@Disabled
+	public void renameToConcreteProject() throws IOException
+	{
+		String concreteProjectName;
+		File sourceProjectDir;
+		File settingsGradle;
+		File dotGithubDir;
+		File codeOfConduct;
+		//
+		concreteProjectName = "foo-bar";
+		sourceProjectDir = PathFinder.getProjectDirectory();
+		settingsGradle = new File(sourceProjectDir, DependenciesData.SETTINGS_GRADLE_FILENAME);
+		ModifyFileExtensions.modifyFile(settingsGradle.toPath(), (count, input) -> {
+			return input.replaceAll(sourceProjectDir.getName(),
+				concreteProjectName)
+				+ System.lineSeparator();
+		});
+
+		dotGithubDir = new File(sourceProjectDir, DependenciesData.DOT_GITHUB_DIRECTORY_NAME);
+		codeOfConduct = new File(dotGithubDir, DependenciesData.CODE_OF_CONDUCT_FILENAME);
+		ModifyFileExtensions.modifyFile(codeOfConduct.toPath(), (count, input) -> {
+			return input.replaceAll(sourceProjectDir.getName(),
+				concreteProjectName)
+				+ System.lineSeparator();
+		});
+	}
 
 }
