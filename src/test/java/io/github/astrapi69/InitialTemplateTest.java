a692c14ac42456948b8fa82b69dedf2fd6384f82
astrapi69 <asterios.raptis@gmx.net>
2021-11-13 14:12:26 +0100

    compile errors fixed

--- src/test/java/io/github/astrapi69/InitialTemplateTest.java
+++ src/test/java/io/github/astrapi69/InitialTemplateTest.java
@@ -24,13 +24,13 @@
  */
 package io.github.astrapi69;
 
-import io.github.astrapi69.delete.DeleteFileExtensions;
+import io.github.astrapi69.file.delete.DeleteFileExtensions;
 import io.github.astrapi69.gradle.migration.data.CopyGradleRunConfigurations;
 import io.github.astrapi69.gradle.migration.data.DependenciesInfo;
 import io.github.astrapi69.gradle.migration.data.GradleRunConfigurationsCopier;
 import io.github.astrapi69.io.file.filter.PrefixFileFilter;
-import io.github.astrapi69.modify.ModifyFileExtensions;
-import io.github.astrapi69.search.PathFinder;
+import io.github.astrapi69.file.modify.ModifyFileExtensions;
+import io.github.astrapi69.file.search.PathFinder;
 import io.github.astrapi69.throwable.RuntimeExceptionDecorator;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
